67b57509a355e117a7c3ed2bdcd5d674c48e7311
Michael L Perry <michael@qedcode.com>
2021-05-02 22:44:38 -0500

    Test the list creation model

--- src/shared/model.test.ts
+++ src/shared/model.test.ts
@@ -1,16 +1,26 @@
 import { Jinaga, JinagaTest } from "jinaga";
+import { authorize, List, User } from "./model";
 
 var j: Jinaga;
 
 beforeEach(() => {
-    j = JinagaTest.create({});
-})
+    j = JinagaTest.create({
+        authorization: authorize,
+        user: new User("---List Creator---")
+    });
+});
 
 test("Can create a list", async () => {
-    const firstFact = await j.fact({
-        type: "MyApplication.FirstFact",
-        identifier: "my_fact_id"
-    });
+    const creator = await j.fact(new User("---List Creator---"));
+    const list = await j.fact(new List(creator));
 
-    expect(firstFact.identifier).toBe("my_fact_id");
+    expect(list.creator.publicKey).toBe("---List Creator---");
+});
+
+test("Can get all lists that a user has created", async () => {
+    const creator = await j.fact(new User("---List Creator---"));
+    const list = await j.fact(new List(creator));
+
+    const lists = await j.query(creator, j.for(List.fromCreator));
+    expect(lists.length).toBe(1);
 });
