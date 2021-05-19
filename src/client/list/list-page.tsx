54fba65d8e94d736d7b4f933e74ee0274a36321d
Jan Verhaegen <jan.verhaegen@tss.be>
2021-05-19 10:21:23 +0200

    Corrected typo

--- src/client/list/list-page.tsx
+++ src/client/list/list-page.tsx
@@ -1,7 +1,7 @@
 import { List } from "@shared/model";
 import * as React from "react";
 import { Link, useParams } from "react-router-dom";
-import { ListContainer } from "./list-comtainer";
+import { ListContainer } from "./list-container";
 
 export const ListPage = ({}) => {
     const { topic } = useParams<{topic: string}>();
