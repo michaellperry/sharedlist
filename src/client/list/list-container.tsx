41752b977955dd0ce5f5091cabbed6269c75a013
Jan Verhaegen <jan.verhaegen@tss.be>
2021-05-19 14:27:20 +0200

    Added list with completed items

--- src/client/list/list-container.tsx
+++ src/client/list/list-container.tsx
@@ -3,15 +3,17 @@ import { ascending, collection, field, jinagaContainer, mapProps, specificationF
 import * as React from "react";
 import { j } from "../jinaga-config";
 import { itemComponent } from "./item-component";
+import { completedItemComponent } from "./completed-item-component";
 
 const listProjection = projectionFor(List, {
     list: field(l => l),
     topic: field(l => l.topic),
-    Items: collection(j.for(Item.inList), itemComponent, ascending(i => i.created))
+    Items: collection(j.for(Item.inList), itemComponent, ascending(i => i.created)),
+    CompletedItems : collection(j.for(Item.inCompletedList), completedItemComponent, ascending(i => i.created)),
 });
 
 export const ListContainer = jinagaContainer(j, mapProps(listProjection).to(
-    ({list, topic, Items}) => {
+    ({list, topic, Items, CompletedItems}) => {
         const [description, setDescription] = React.useState("");
 
         const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -34,6 +36,8 @@ export const ListContainer = jinagaContainer(j, mapProps(listProjection).to(
                     <input type="submit" value="Add" />
                 </form>
                 <Items />
+                <h3>Completed items:</h3>
+                <CompletedItems />
             </>
         );
     }
