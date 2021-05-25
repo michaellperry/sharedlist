995f013fa3ff9dd15dfcd8922dea679fe6fb0cb0
Michael L Perry <michael@qedcode.com>
2021-05-24 21:04:41 -0500

    Output the results of a query

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -15,7 +15,23 @@ import { j } from "./jinaga-config";
     //     description: "Daleks",
     //     created: new Date()
     // });
+
+    // const items = await j.query(list, j.for(itemsInList));
+    // display(items);
 })();
+
+function itemsInList(list: any) {
+    return j.match({
+        type: "SharedList.Item",
+        list
+    });
+}
+
+function display(obj: any) {
+    const output = document.createElement("pre");
+    output.innerText = JSON.stringify(obj, null, 4);
+    document.getElementById("output-host")?.appendChild(output);
+}
 
 ReactDOM.render(
     <App />,
