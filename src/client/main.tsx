23e66335e4dddd8bc86bc57c9557d7cda32044d2
Michael L Perry <michael@qedcode.com>
2021-05-24 20:37:29 -0500

    Demonstrate creating a fact

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -3,10 +3,19 @@ import * as ReactDOM from "react-dom";
 import { App } from "./app";
 import { j } from "./jinaga-config";
 
-j.fact({
-    type: "MyApplication.Visit",
-    date: new Date()
-});
+(async () => {
+    // const list = await j.fact({
+    //     type: "SharedList.List",
+    //     topic: "villains"
+    // });
+
+    // const item = await j.fact({
+    //     type: "SharedList.Item",
+    //     list,
+    //     description: "Daleks",
+    //     created: new Date()
+    // });
+})();
 
 ReactDOM.render(
     <App />,
