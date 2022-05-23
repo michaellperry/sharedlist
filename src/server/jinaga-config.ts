b06ccd1e7b38d3d8dca5ee0cd4fc33fb712658be
Michael L Perry <michael@qedcode.com>
2022-05-23 17:21:23 -0500

    Point to demo server

--- src/server/jinaga-config.ts
+++ src/server/jinaga-config.ts
@@ -4,5 +4,5 @@ import { JinagaServer } from "jinaga";
 export function configureJinaga(app: express.Express) {
     const { handler } = JinagaServer.create({});
 
-    app.use('/jinaga', handler);
+    app.use('https://jinaga-sharedlist.azurewebsites.net/jinaga', handler);
 }
