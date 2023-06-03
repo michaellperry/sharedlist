594826bb7ffa56971be13562ba214db8ebf3552f
Michael L Perry <michael@qedcode.com>
2023-06-02 19:47:00 -0500

    Install session and cookie parser

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,11 +1,13 @@
 import { json } from "body-parser";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, { urlencoded } from "express";
+import session from "express-session";
 import http from "http";
+import { configureAuthentication } from "./authentication/authentication";
 import { configureJinaga } from "./jinaga-config";
 import { configureRoutes } from "./routes";
 import { startTracing, traceInfo } from "./tracing";
-import { configureAuthentication } from "./authentication/authentication";
 
 startTracing();
 
@@ -13,6 +15,12 @@ const app = express();
 const server = http.createServer(app);
 
 app.set("port", process.env.PORT || 8080);
+app.use(session({
+    secret: process.env.SESSION_SECRET || 'tacocat',
+    resave: false,
+    saveUninitialized: true
+  }));
+app.use(cookieParser());
 app.use(urlencoded());
 app.use(json());
 app.use(cors());
